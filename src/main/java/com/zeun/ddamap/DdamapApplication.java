b7f5706a8d5244103df300282e58da8d44c0357d
ddzeun <ddzeun@gmail.com>
2025-06-15 23:29:01 +0900

    Initial commit

---
+++ src/main/java/com/zeun/ddamap/DdamapApplication.java
@@ -0,0 +1,13 @@
+package com.zeun.ddamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdamapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdamapApplication.class, args);
+	}
+
+}
