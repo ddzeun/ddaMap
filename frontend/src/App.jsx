c515ad60d8250f51d7f2c4dfce9e0caf9e937159
ddzeun <ddzeun@gmail.com>
2025-07-03 19:30:35 +0900

    feat(fe): react-naver-maps 라이브러리를 활용한 네이버 지도 렌더링
    - react-naver-maps 라이브러리 설치
    - 라이브러리 사용을 위한 react 버전 변경(18.2.0)
    - components/MapContainer에 지도 랜더링 부분 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect } from 'react';
 import { getAllStations } from './api/stationApi';
+import MapContainer from './components/MapContainer';
+import { NavermapsProvider } from 'react-naver-maps';
 
 function App() {
+
+  const naverMapsClientId = import.meta.env.VITE_NAVER_MAP_CLIENT_ID;
+
 
   useEffect(() => {    
     const fetchStations = async () => {
@@ -16,10 +21,12 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1>따맵</h1>
-
-    </>
+    <NavermapsProvider ncpKeyId={ naverMapsClientId }>
+      <div style={{ width: '100vw', height: '100vh' }} >
+        <h1>따맵</h1>
+        <MapContainer />
+      </div>
+    </NavermapsProvider>
   )
 }
 
